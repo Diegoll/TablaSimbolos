97fd4074ec49dbf07fa80033febb1b00b19a54d2
Marci <Marci@Mark2>
2013-12-15 16:52:50 +0100

    Añadidos los getNombres

--- TablaSimbolos.c
+++ TablaSimbolos.c
@@ -6,13 +6,39 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+#include "TablaSimbolos.h"
 
 /*
  * 
  */
-int main(int argc, char** argv) {
 
+char* getNombreTipoNodo(TSNodo n){
+    switch (n->entrada){
+        case marca: return "marca";
+        case funcion: return "funcion";
+        case variable: return "variable";
+        case tipo_person: return "tipo_person";
+        case parametro_formal: return "parametro_formal";
+    }
+}
+char* getNombreDatoNodo(TSNodo n){
+    switch(n->tipo){
+        case tstring: return "tstring";
+        case tint: return "tint";
+        case tchar: return "tchar";
+        case tfloat: return "tfloat";
+        case tboolean: return "tboolean";
+        case tstack: return "tstack";
+        case not_assigned: return "not_assigned";
+        case unknown: return "unknown";
+    }
+}
+char* getNombreTipoDatoNodo(TSNodo n){
+
+}
+int main(int argc, char** argv) {
+    
     return (EXIT_SUCCESS);
 }
 
